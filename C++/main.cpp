460bcee7726a3b5f7d8b86cabd87bac9836eb91a
Shekhar Chaudhary <2307shekhar@gmail.com>
2018-08-17 23:16:48 +0530

    Initial Commit

---
+++ C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by Shekhar Chaudhary on 17/08/18.
+//  Copyright © 2018 Shekhar Chaudhary. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
